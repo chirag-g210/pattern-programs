6fab62ac01d8901b4f4ea3e72ac7f7663f6708b8
Chirag Gupta <piyushg.2648@gmail.com>
2026-02-12 00:24:49 +0530

    Add star patterns

---
+++ star_patterns/Square.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class Square{
+    public static void squarePattern(int n){
+        for(int i = 0 ; i < n+1 ; i++){
+            for(int j = 0 ; j < n+1 ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();   //side of square..
+        squarePattern(n);
+    }
+}
+
