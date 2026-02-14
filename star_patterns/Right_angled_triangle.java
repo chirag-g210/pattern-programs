8edfcd4f699c735c14fcb01e8d5d97245d7efae1
Chirag Gupta <piyushg.2648@gmail.com>
2026-02-14 13:09:20 +0530

    Add right-angled triangle star pattern class
    Implement a right-angled triangle star pattern generator.

---
+++ star_patterns/Right_angled_triangle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class right_angled {
+    public static void pattern(int n){
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j < i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern(n);
+    }
+}
+
+/* output
+*
+* *
+* * *
+* * * *
+* * * * *
+ */
