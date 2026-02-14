57b469af543a954dc861373a7ad8b915febe452f
Chirag Gupta <piyushg.2648@gmail.com>
2026-02-14 15:06:25 +0530

    did a inverted pattern using for loop

---
+++ star_patterns/inverted_right_angled.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class inverted_right_angled {
+    public static void pattern(int n){
+        for(int i = 0; i <= n ; i++){
+            for(int j = n ; j > i ; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern(n);
+    }
+}
+
+/*
+---> output for n = 5
+
+        * * * * * 
+        * * * * 
+        * * * 
+        * *
+        *
+
+*/
