e9cdd7d0b29cced168799a751b031848814a5999
Chirag Gupta <piyushg.2648@gmail.com>
2026-02-15 19:57:18 +0530

    Add simple number pattern generator
    Implement a simple number pattern generator that prints numbers in a triangular format based on user input.

---
+++ number_patterns/simple.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class simple {
+    public static void pattern(int n){
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= i ;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        pattern(n);        
+    }
+}
+
+
+/*
+      sample output for n = 5
+          1
+          12
+          123
+          1234
+          12345
+          
+          */
